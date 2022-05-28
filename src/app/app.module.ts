e9bbe7b4b52b9c1586f283806b10f579ade4e07b
Alexander Kuznetsov <alkuznetsov@aol.com>
2022-05-28 23:25:20 +0500

    Update Angular from 10 to 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     CartDetailsComponent
   ],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     BrowserModule,
     HttpClientModule,
     NgbModule
